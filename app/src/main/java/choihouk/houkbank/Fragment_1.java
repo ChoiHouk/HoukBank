4ea8245b2aeb060172613f5724754fec3edb2d99
choihouk <choihoug@gmail.com>
2017-11-15 10:47:07 +0900

    mypage layout fix

--- app/src/main/java/choihouk/houkbank/Fragment_1.java
+++ app/src/main/java/choihouk/houkbank/Fragment_1.java
@@ -1,15 +1,43 @@
 package choihouk.houkbank;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class Fragment_1 extends Fragment {
 
+    ViewGroup rootView;
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_1, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = (ViewGroup) inflater.inflate(R.layout.fragment_1, container, false);
+
+        ImageButton adButton = (ImageButton) rootView.findViewById(R.id.ad_btn);
+
+        adButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://kmong.com/")));
+
+            }
+        });
+
+
+        return rootView;
+    }
+
 }
